0373875c2ca578a1210fa26370febf152f548bd5
Philix Hein <159520074+PhilixTheExplorer@users.noreply.github.com>
2024-03-01 15:59:41 +0700

    Update Solution1.java

--- Lab2_26Jan24/Solution1.java
+++ Lab2_26Jan24/Solution1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class Main {
+public class Solution1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] input = sc.nextLine().toLowerCase().split(" ");
