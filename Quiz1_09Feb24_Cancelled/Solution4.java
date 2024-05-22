040c707f0c7b41f0fda5e94c2b0c101998d0b393
Philix Hein <159520074+PhilixTheExplorer@users.noreply.github.com>
2024-05-22 20:20:30 +0700

    Update Solution4.java

--- Quiz1_09Feb24_Cancelled/Solution4.java
+++ Quiz1_09Feb24_Cancelled/Solution4.java
@@ -2,23 +2,20 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class Solution4 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            String number = String.valueOf(sc.nextInt());
-            HashSet<Character> set = new HashSet<>();
-            boolean isUnique = true;
-            for (int j = 0; j < number.length(); j++) {
-                if (!set.add(number.charAt(j))) {
-                    isUnique = false;
-                }
-            }
-            if (isUnique)
-                count++;
-        }
-        System.out.println(count);
-        sc.close();
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+      String number = sc.next();
+      HashSet<Character> set = new HashSet<>();
+      for (int j = 0; j < number.length(); j++) {
+        set.add(number.charAt(j));
+      }
+      if (set.size() == number.length()) count++;
     }
+    System.out.println(count);
+    sc.close();
+  }
 }
