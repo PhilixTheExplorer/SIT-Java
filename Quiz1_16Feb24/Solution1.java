46ca43b7155d9874a74b30642e85db99d311e361
Philix Hein <159520074+PhilixTheExplorer@users.noreply.github.com>
2024-05-22 19:52:23 +0700

    Update Solution1.java

--- Quiz1_16Feb24/Solution1.java
+++ Quiz1_16Feb24/Solution1.java
@@ -1,35 +1,34 @@
 import java.util.Scanner;
 
 public class Solution1 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String password = sc.next();
-        boolean isValid = false;
-        if (password.length() < 8 || password.length() > 15) {
-            isValid = false;
-        } else {
-            boolean containsLowerCase = false;
-            boolean containsUpperCase = false;
-            boolean containsDigit = false;
-            boolean containsSpecialChar = false;
 
-            String specialChars = "@#%&!$*";
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String password = sc.next();
+    boolean isValid = false;
+    if (password.length() >= 8 && password.length() <= 15) {
+      boolean containsLowerCase = false;
+      boolean containsUpperCase = false;
+      boolean containsDigit = false;
+      boolean containsSpecialChar = false;
 
-            for (char ch : password.toCharArray()) {
-                if (Character.isLowerCase(ch))
-                    containsLowerCase = true;
-                else if (Character.isUpperCase(ch))
-                    containsUpperCase = true;
-                else if (Character.isDigit(ch))
-                    containsDigit = true;
-                else if (specialChars.indexOf(ch) != -1)
-                    containsSpecialChar = true;
-            }
+      String specialChars = "@#%&!$*";
 
-            if (containsLowerCase && containsUpperCase && containsDigit && containsSpecialChar)
-                isValid = true;
-        }
-        sc.close();
-        System.out.println(isValid);
+      for (char ch : password.toCharArray()) {
+        if (Character.isLowerCase(ch)) containsLowerCase = true;
+        else if (Character.isUpperCase(ch)) containsUpperCase = true;
+        else if (Character.isDigit(ch)) containsDigit = true;
+        else if (specialChars.indexOf(ch) != -1) containsSpecialChar = true;
+      }
+
+      if (
+        containsLowerCase &&
+        containsUpperCase &&
+        containsDigit &&
+        containsSpecialChar
+      ) isValid = true;
     }
+    sc.close();
+    System.out.println(isValid);
+  }
 }
