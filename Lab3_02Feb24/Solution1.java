38196f7d344c58fb2e699c845a97605b29ab9599
Philix Hein <159520074+PhilixTheExplorer@users.noreply.github.com>
2024-08-26 01:19:08 +0700

    Update Solution1.java

--- Lab3_02Feb24/Solution1.java
+++ Lab3_02Feb24/Solution1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class cross {
+public class Solution1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
